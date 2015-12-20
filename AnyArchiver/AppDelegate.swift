1e55662c7610e8af9dd5742dab01934cc8d4b922
Lk Xf <elveestei@gmail.com>
2015-12-20 22:18:08 +0100

    Initial Commit

---
+++ AnyArchiver/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AnyArchiver
+//
+//  Created by Tamas Lustyik on 2015. 12. 20..
+//  Copyright © 2015. Tamas Lustyik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
